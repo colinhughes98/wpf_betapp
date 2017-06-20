6b97275cbd5f69f88b099bd32763c17189fe10a1
colinhughes98 <colinhughes98@gmail.com>
2017-06-20 21:06:37 +0100

    WPF

---
+++ BettingApp/Bootstrapper.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Prism.Modularity;
+using Prism.Unity;
+
+
+namespace BettingApp
+{
+    public class Bootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return new Shell();
+        }
+        protected override void InitializeShell()
+        {
+            base.InitializeShell();
+            Application.Current.MainWindow = (Window) Shell;
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureModuleCatalog()
+        {
+            base.ConfigureModuleCatalog();
+            ModuleCatalog moduleCatalog = (ModuleCatalog)ModuleCatalog;
+            moduleCatalog.AddModule(typeof(Fixtures.FixturesModule));
+        }
+    }
+}
---
+++ Fixtures/FixturesModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Prism.Modularity;
+using Prism.Regions;
+
+namespace Fixtures
+{
+    public class FixturesModule : IModule
+    {
+        private readonly IRegionManager _regionManager;
+        public FixturesModule(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+        }
+        public void Initialize()
+        {
+            _regionManager.RegisterViewWithRegion("MainRegion", typeof(Views.HelloWorld));
+        }
+    }
+}
